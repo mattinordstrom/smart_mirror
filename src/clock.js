6467ee7392aef9b34ded22594a03c7bdfd513fb3
Matti Nordstrom <matti.nordstrom@gmail.com>
2017-02-04 20:38:51 +0100

    Some changes to the UI to make the text easier to read

--- src/clock.js
+++ src/clock.js
@@ -1,4 +1,7 @@
 function setTime() {
+	var dayStrings = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	var monthStrings = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+	
 	var today = new Date();
 	var h = today.getHours();
 	var m = today.getMinutes();
@@ -6,6 +9,7 @@ function setTime() {
 		m = "0" + m;
 	}
 	$('#div_time :first').html(h + ":" + m);
-	$('#div_time :eq(1)').html(today.toDateString());
+	$('#div_time :eq(1)').html(dayStrings[today.getDay()]);
+	$('#div_time :eq(2)').html(today.getDate() + " " + monthStrings[today.getMonth()] + " " + today.getFullYear());
 	setTimeout(setTime, 500);
 }
