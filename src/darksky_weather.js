3ebada3c65ec839bc25391860fcc96eb88e71cc4
Matti Nordstrom <matti.nordstrom@gmail.com>
2017-02-04 22:07:19 +0100

    Minor position and title size fix in UI

--- src/darksky_weather.js
+++ src/darksky_weather.js
@@ -15,7 +15,7 @@ function handleWeather(data) {
 	skycons.set("weather_icon", Skycons[data.forecast.currently.icon.toUpperCase().replace(/-/g, '_')]);
 	skycons.play();
 	
-	$('#div_weather h1:first').html(data.forecast.currently.temperature.toFixed(1) + "&deg;");
+	$('#div_weather h2:first').html(data.forecast.currently.temperature.toFixed(1) + "&deg;");
 	$('#div_weather :eq(1)').html(data.forecast.currently.summary);
 	$('#weatherCoords').html("Longitude: " + data.forecast.longitude + "<br/>Latitude: " + data.forecast.latitude);
 		
